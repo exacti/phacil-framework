3e01ef1ef90b83489b3bbcf317508c5e593175e1
Andersom Perozzi <hospedagem@exacti.com.br>
2019-02-25 22:56:11 -0300

    Seo URL wildcard algoritm change

--- system/url/seo_url.php
+++ system/url/seo_url.php
@@ -17,41 +17,41 @@ class SystemUrlSeoUrl extends Controller {
     }
 
     public function index() {
-		// Add rewrite to url class
-		if ($this->config->get('config_seo_url')) {
-			$this->url->addRewrite($this);
-		}
+        // Add rewrite to url class
+        if ($this->config->get('config_seo_url')) {
+            $this->url->addRewrite($this);
+        }
 
-		$match = array();
-		
-		// Decode URL
-		if (isset($this->request->get['_route_'])) {
-			//$parts = explode('/', $this->request->get['_route_']);
-			$parts = array($this->request->get['_route_']);
+        $match = array();
 
-			foreach ($parts as $part) {
-			    if(defined('USE_DB_CONFIG') && USE_DB_CONFIG == true)
-			        $query = $this->db->query("SELECT * FROM url_alias WHERE keyword = '" . $this->db->escape($part) . "'");
+        // Decode URL
+        if (isset($this->request->get['_route_'])) {
+            $parts_count = explode('/', $this->request->get['_route_']);
+            $parts = array($this->request->get['_route_']);
 
-				if (isset($query) && $query != false && $query->num_rows === 1) {
+            foreach ($parts as $part) {
+                if(defined('USE_DB_CONFIG') && USE_DB_CONFIG == true)
+                    $query = $this->db->query("SELECT * FROM url_alias WHERE keyword = '" . $this->db->escape($part) . "'");
 
-					//$url = explode('=', $query->row['query']);
+                if (isset($query) && $query != false && $query->num_rows === 1) {
 
-					if($query->row['get'] != "") {
-						$a = explode(',', $query->row['get']);
+                    //$url = explode('=', $query->row['query']);
 
-						foreach($a as $value) {
-							$b = explode('=', $value);
-							$_GET[$b[0]] = $b[1];
-						}
-					}
-					//var_dump($query->row['query']);
-					$this->request->get['route'] = $query->row['query'];
+                    if($query->row['get'] != "") {
+                        $a = explode(',', $query->row['get']);
 
-				} elseif (defined('ROUTES') && is_array(ROUTES)) {
-				    $rotas = ROUTES;
+                        foreach($a as $value) {
+                            $b = explode('=', $value);
+                            $_GET[$b[0]] = $b[1];
+                        }
+                    }
 
-				    if(isset($rotas[$part])){
+                    $this->request->get['route'] = $query->row['query'];
+
+                } elseif (defined('ROUTES') && is_array(ROUTES)) {
+                    $rotas = ROUTES;
+
+                    if(isset($rotas[$part])){
                         $this->request->get['route'] = $rotas[$part];
                     } else {
 
@@ -64,11 +64,20 @@ class SystemUrlSeoUrl extends Controller {
                                 $preg = str_replace(array_keys($this->regType), array_values($this->regType), $preg);
 
                                 if((@preg_match("/". $preg . "/", $parts[0], $match))) {
-                                    unset($match[0]);
 
-                                    $match = $this->request->clean($match);
+                                    $countTree = explode("/", $match[0]);
 
-                                    $pagina = $page;
+                                    if(count($countTree) == count($parts_count)){
+
+                                        unset($match[0]);
+
+                                        $match = $this->request->clean($match);
+
+                                        $pagina = $page;
+
+                                        break;
+                                    }
+
                                 }
                             }
                         }
@@ -81,87 +90,93 @@ class SystemUrlSeoUrl extends Controller {
                     }
 
                 } else {
-					$this->request->get['route'] = $this->notfound;
-				}
-			}
+                    $this->request->get['route'] = $this->notfound;
+                }
+            }
 
 
-			if (isset($this->request->get['route'])) {
-				return $this->forward($this->request->get['route'], $match);
-			}
-		}
-	}
-	
-	public function rewrite($link) {
-		if ($this->config->get('config_seo_url')) {
-			$url_data = parse_url(str_replace('&amp;', '&', $link));
-		
-			$url = ''; 
-			
-			$data = array();
-			
-			parse_str($url_data['query'], $data);
+            if (isset($this->request->get['route'])) {
+                return $this->forward($this->request->get['route'], $match);
+            }
+        }
+    }
 
-			$joinRegex = implode("|", array_keys($this->regType));
+    public function rewrite($link) {
+        if ($this->config->get('config_seo_url')) {
+            $url_data = parse_url(str_replace('&amp;', '&', $link));
 
-			foreach ($data as $key => $value) {
+            $url = '';
 
-				if (isset($data['route'])) {
+            $data = array();
+
+            parse_str($url_data['query'], $data);
+
+            $joinRegex = implode("|", array_keys($this->regType));
+
+            foreach ($data as $key => $value) {
+
+                if (isset($data['route'])) {
                     if(defined('USE_DB_CONFIG') && USE_DB_CONFIG == true)
                         $query = $this->db->query("SELECT * FROM url_alias WHERE `query` = '" . $this->db->escape($value) . "'");
-					if (isset($query) && $query->num_rows && $query->num_rows != NULL) {
-						$url .= '/' . $query->row['keyword'];
-					} elseif (defined('ROUTES') && is_array(ROUTES)) {
-					    $arV = array_search($value, ROUTES);
-					    if($arV){
+                    if (isset($query) && $query->num_rows && $query->num_rows != NULL) {
+                        $url .= '/' . $query->row['keyword'];
+                    } elseif (defined('ROUTES') && is_array(ROUTES)) {
+                        //$arV = array_search($value, ROUTES);
+                        foreach(ROUTES as $query => $page) {
+                            if($page == $value){
+                                if(isRegularExpression($query)){
+                                    unset($data['route']);
+                                    $qnt = substr_count($query, '%');
+                                    if(count($data) == $qnt) {
+                                        $str = $query;
 
-					        if(isRegularExpression($arV)){ //check is a valid wildcard
+                                        foreach($data as $replace){
+                                            $str = preg_replace('/('.$joinRegex.')/', $replace, $str, 1);
+                                        }
 
-                                unset($data['route']);
-                                $str = $arV;
+                                        $url .= '/' .($str);
 
-                                foreach($data as $replace){
-                                    $str = preg_replace('/('.$joinRegex.')/', $replace, $str, 1);
+                                        $data = false;
+
+                                        break;
+                                    }
+                                } else {
+                                    $url .= '/' . (array_search($value, ROUTES));
                                 }
 
-                                $url .= '/' .($str);
-
-                                $data = false;
-
-                            } else {
-                                $url .= '/' . (array_search($value, ROUTES));
                             }
 
                         }
+
                     }
-					unset($data[$key]);
-				}
+                    unset($data[$key]);
+                }
 
-			}
-		
-			if ($url) {
-				unset($data['route']);
-			
-				$query = '';
-			
-				if ($data) {
-					foreach ($data as $key => $value) {
-						$query .= '&' . $key . '=' . $value;
-					}
-					
-					if ($query) {
-						$query = '?' . trim($query, '&');
-					}
-				}
+            }
 
-				return $url_data['scheme'] . '://' . $url_data['host'] . (isset($url_data['port']) ? ':' . $url_data['port'] : '') . str_replace('/index.php', '', $url_data['path']) . $url . $query;
-			} else {
-				return $link;
-			}
-		} else {
-			return $link;
-		}		
-	}	
+            if ($url) {
+                unset($data['route']);
+
+                $query = '';
+
+                if ($data) {
+                    foreach ($data as $key => $value) {
+                        $query .= '&' . $key . '=' . $value;
+                    }
+
+                    if ($query) {
+                        $query = '?' . trim($query, '&');
+                    }
+                }
+
+                return $url_data['scheme'] . '://' . $url_data['host'] . (isset($url_data['port']) ? ':' . $url_data['port'] : '') . str_replace('/index.php', '', $url_data['path']) . $url . $query;
+            } else {
+                return $link;
+            }
+        } else {
+            return $link;
+        }
+    }
 }
 
 function isRegularExpression($string) {
